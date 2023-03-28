02d249e8f2e20fcadabe3bf5fded89cb831d3d10
Owen Kosman <owenkosman@Owens-MacBook-Pro-3.local>
2023-03-28 23:34:06 +0100

    refined ui

--- app/header.tsx
+++ app/header.tsx
@@ -6,7 +6,7 @@ import { usePathname } from "next/navigation";
 const navigation = [
   {
     name: "GitHub",
-    href: "https://github.com/chronark/envshare",
+    href: "https://github.com/Natto-boys",
     external: true,
   },
 ] satisfies { name: string; href: string; external?: boolean }[];
@@ -14,9 +14,9 @@ const navigation = [
 export const Header: React.FC = () => {
   const pathname = usePathname();
   return (
-    <header className="top-0 z-30 w-full px-4 sticky drop-shadow-md backdrop-blur bg-violet-50/50">
+    <header className="top-0 z-30 w-full px-4 sticky backdrop-blur border border-t border-zinc-500/20 bg-violet-50/50">
       <div className="container mx-auto">
-        <div className="flex flex-col items-center justify-between gap-2 pt-6 sm:h-14 sm:flex-row sm:pt-0">
+        <div className="flex items-center justify-between sm:h-14 flex-row sm:pt-0">
           <Link href="/" className="text-xl font-semibold duration-150 text-zinc-900 hover:text-zinc-800">
             HingeGPT
           </Link>
@@ -26,7 +26,7 @@ export const Header: React.FC = () => {
               {navigation.map((item) => (
                 <li className="" key={item.href}>
                   <Link
-                    className={`flex items-center px-3 py-2 duration-150 text-sm sm:text-base  hover:text-zinc-50
+                    className={`flex items-center px-3 py-2 duration-150 text-sm sm:text-base  hover:text-zinc-800
                     ${pathname === item.href ? "text-zinc-200" : "text-zinc-400"}`}
                     href={item.href}
                     target={item.external ? "_blank" : undefined}
