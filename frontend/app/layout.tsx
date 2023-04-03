35eb0ac75edb2eed27c3e7d30009b3a2d1165122
Owen Kosman <owenkosman@Owens-MacBook-Pro-3.local>
2023-04-03 09:14:18 +0100

    added analytics

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,11 @@
 import "./globals.css";
 import { Inter } from "@next/font/google";
 import Link from "next/link";
+<<<<<<< Updated upstream
+=======
+import Script from "next/script";
+import { Header } from "./header";
+>>>>>>> Stashed changes
 
 import { Analytics } from "app/components/analytics";
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
@@ -13,6 +18,22 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <head />
+      <Script id="google-tag-manager" strategy="afterInteractive">
+      {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-THJWF6M');
+      `}
+      </Script>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-0YJPW2Z1GB"></Script>
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-0YJPW2Z1GB');`}
+      </Script>
       <body className="relative min-h-screen bg-zinc-100">
         {
           // Not everyone will want to host envshare on Vercel, so it makes sense to make this opt-in.
