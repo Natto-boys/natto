6884b661dde47736b008a1d7b9435669a65364b9
jammygrams <jmahbubani@gmail.com>
2023-04-05 20:17:56 +0100

    file size error message

--- backend/app.py
+++ backend/app.py
@@ -4,11 +4,12 @@ import asyncio
 import websockets
 from typing import Dict
 from websockets.server import WebSocketServerProtocol
+from websockets.exceptions import PayloadTooBig
 from SimpleChatBridge import SimpleChatBridge
 from OCR import OCR
 
 HTTP_SERVER_PORT = 8080
-
+MAX_SIZE = 2*(2 ** 20) # 2 MB
 
 async def respond(ws: WebSocketServerProtocol):
     async def on_response(response: Dict[str, str]) -> None:
@@ -35,34 +36,40 @@ async def respond(ws: WebSocketServerProtocol):
     ocr = OCR()
 
     async for message in ws:
-        data = json.loads(message)
+        try:
+            data = json.loads(message)
 
-        if data.get("event") is None:
+            if data.get("event") is None:
+                await on_error_response(
+                    "Invalid object. Refer to our documentation for more details."
+                )
+                continue
+
+            if data["event"] == "text":
+                if data.get("text") is None or data.get("name") is None:
+                    await on_error_response("Missing 'text' or 'name' field")
+                    continue
+                chatBridge.generate_messages(input=data["text"], name=data["name"])
+                await chatBridge.send_chat()
+
+            if data["event"] == "image":
+                if data.get("content") is None:
+                    await on_error_response("Missing 'content' field")
+                    continue
+                content = ocr.base64_to_bytes(data["content"])
+                text = ocr.get_prompt_text(content)
+                await on_image_response(text)
+        except PayloadTooBig as e:
             await on_error_response(
-                "Invalid object. Refer to our documentation for more details."
+                f"Filesize too large, max size is {MAX_SIZE / (2 ** 20)} MB"
             )
             continue
-
-        if data["event"] == "text":
-            if data.get("text") is None or data.get("name") is None:
-                await on_error_response("Missing 'text' or 'name' field")
-                continue
-            chatBridge.generate_messages(input=data["text"], name=data["name"])
-            await chatBridge.send_chat()
-
-        if data["event"] == "image":
-            if data.get("content") is None:
-                await on_error_response("Missing 'content' field")
-                continue
-            content = ocr.base64_to_bytes(data["content"])
-            text = ocr.get_prompt_text(content)
-            await on_image_response(text)
 
     print("WS connection closed")
 
 
 async def main(port: int = HTTP_SERVER_PORT) -> None:
-    async with websockets.serve(respond, None, port, max_size=2*(2 ** 20)): # 2 MB max size
+    async with websockets.serve(respond, None, port, max_size=MAX_SIZE):
         print(f"server listening on: http://localhost:{port}")
         await asyncio.Future()
 
