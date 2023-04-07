20d905ccae9e795116a7b2f1fef2e7168fb8025a
Owen Kosman <owenkosman@Owens-MacBook-Pro-3.local>
2023-04-07 20:03:20 +0100

    ui improvements again

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -196,83 +196,84 @@ export default function Home() {
             onSubmit();
           }}
         >
-          
-          <div className="flex items-center w-full h-16 py-2 px-1 bg-transparent justify-between focus-within:border-zinc-100/80 focus-within:ring-0">
-            
-            <input type="name" ref={nameRef} value={name} onChange={(e) => handleName(e.target.value)} className="duration-150 w-3/5 pl-2 bg-transparent border-none text-zinc-900 focus:ring-0 text-2xl font-semibold" />
-            <button type="button" onClick={handleNameFocus} className="flex items-center p-2 rounded-md hover:bg-zinc-900/10">
-              <PencilIcon className="w-5 h-5" />
-            </button>
-          </div>
-            <div className="flex chat chat-start justify-between items-center px-1 text-sm">
-              <div className="flex gap-2 flex-col justify-end">
-                {isUpload ? 
-                <div className="chat-bubble mt-6 w-3/8 inline-flex justify-center items-center bg-white">
-                  <div className="loader-dark" />
-                </div> : 
-                <>
-                <button type="button" onClick={handleImageFocus} className={`rounded-full h-12 text-lg w-[90%] bg-violet-300 text-black hover:bg-violet-900/10 ${loadingClassname}`}>
-                  Use screenshot
+          <div className="flex flex-col">
+                <button type="button" onClick={handleImageFocus} className={`rounded-full w-4/5 mb-8 h-12 self-center text-lg bg-violet-600 text-white hover:bg-violet-900/30 ${loadingClassname}`}>
+                  Upload screenshot
                 </button>
                 <input ref={fileRef} type="file" accept="image/png, image/jpeg" className="hidden" onChange={(e) => handleChange(e)} />
-                <textarea
-                  id="prompt"
-                  ref={promptRef}
-                  name="text"
-                  value={text}
-                  minLength={1}
-                  onChange={(e) => setText(e.target.value)}
-                  rows={Math.max(5, text.split("\n").length)}
-                  className="w-full chat-bubble bg-white font-serif bg-transparent border-0 appearance-none p-6 resize-none hover:resize text-zinc-900 placeholder-zinc-500 focus:ring-0 text-2xl"
-                />
-                </>
-                  }
-              </div>
-              
-              <div className="flex flex-col gap-4">
-                <button type="button" onClick={handlePromptFocus} className="flex items-end p-2 rounded-md hover:bg-zinc-900/10">
-                  <PencilIcon className="w-5 h-5" />
-                </button>
-              </div>
-              
+                <p className="flex text-center horizontal-line text-sm w-full text-zinc-700">OR</p>
+                <div className="flex items-center mt-4 w-full h-16 py-2 px-1 bg-transparent justify-between focus-within:border-zinc-100/80 focus-within:ring-0">
+              <input type="name" ref={nameRef} value={name} onChange={(e) => handleName(e.target.value)} className="duration-150 w-3/5 pl-2 bg-transparent border-none text-zinc-900 focus:ring-0 text-2xl font-semibold" />
+              <button type="button" onClick={handleNameFocus} className="flex items-center p-2 rounded-md hover:bg-zinc-900/10">
+                <PencilIcon className="w-5 h-5" />
+              </button>
             </div>
-            {serverRes ? 
-            <div className="flex chat chat-end flex-col items-center justify-center w-full gap-4 mt-4 sm:flex-row">
-                <div className="flex flex-row items-center chat-bubble duration-150 text-zinc-100 bg-violet-700 font-light focus:ring-0 text-lg">
-                  {serverRes}
-                  <button type="button" onClick={handleCopy} className="flex items-end p-2 rounded-md hover:bg-zinc-900/10">
-                    <DocumentDuplicateIcon className="w-5 h-5" />
+              <div className="flex chat chat-start justify-between items-center px-1 text-sm">
+                <div className="flex gap-2 flex-col justify-end">
+                  {isUpload ? 
+                  <div className="chat-bubble mt-6 w-3/8 inline-flex justify-center items-center bg-white">
+                    <div className="loader-dark" />
+                  </div> : 
+                  <>
+                  <textarea
+                    id="prompt"
+                    ref={promptRef}
+                    name="text"
+                    value={text}
+                    minLength={1}
+                    onChange={(e) => setText(e.target.value)}
+                    rows={Math.max(5, text.split("\n").length)}
+                    className="w-full chat-bubble bg-white font-serif bg-transparent border-0 appearance-none p-6 resize-none hover:resize text-zinc-900 placeholder-zinc-500 focus:ring-0 text-2xl"
+                  />
+                  </>
+                    }
+                </div>
+                
+                <div className="flex flex-col gap-4">
+                  <button type="button" onClick={handlePromptFocus} className="flex items-end p-2 rounded-md hover:bg-zinc-900/10">
+                    <PencilIcon className="w-5 h-5" />
                   </button>
                 </div>
                 
-            </div> : <></>}
-            <div className="chat chat-end">
-              {loading ? 
-              <div className="w-3/8 chat-bubble mt-6 w-3/8 inline-flex justify-center items-center bg-violet-700">
-                <div className="loader" />
-                </div> :
-                <button
-                  type="submit"
-                  disabled={isDisabled()}
-                  className={`mt-6 w-3/8 h-12 inline-flex justify-center items-center transition-all rounded-full px-4 py-1.5 md:py-2 text-base font-semibold leading-7 bg-zinc-200 ring-1 ring-transparent duration-150   ${
-                    isDisabled()
-                      ? "text-zinc-400 cursor-not-allowed"
-                      : "text-zinc-900 hover:text-zinc-100  hover:bg-zinc-900/40"
-                  }`}
-                >
-                  <span>Generate</span>
-                </button>
-              }
-            </div>
+              </div>
+              {serverRes ? 
+              <div className="flex chat chat-end flex-col items-center justify-center w-full gap-4 mt-4 sm:flex-row">
+                  <div className="flex flex-row items-center chat-bubble duration-150 text-zinc-100 bg-violet-700 font-light focus:ring-0 text-lg">
+                    {serverRes}
+                    <button type="button" onClick={handleCopy} className="flex items-end p-2 rounded-md hover:bg-zinc-900/10">
+                      <DocumentDuplicateIcon className="w-5 h-5" />
+                    </button>
+                  </div>
+                  
+              </div> : <></>}
+              <div className="chat chat-end">
+                {loading ? 
+                <div className="w-3/8 chat-bubble mt-6 w-3/8 inline-flex justify-center items-center bg-violet-700">
+                  <div className="loader" />
+                  </div> :
+                  <button
+                    type="submit"
+                    disabled={isDisabled()}
+                    className={`mt-6 w-3/8 h-12 inline-flex justify-center items-center transition-all rounded-full px-4 py-1.5 md:py-2 text-base font-semibold leading-7 bg-zinc-200 ring-1 ring-transparent duration-150   ${
+                      isDisabled()
+                        ? "text-zinc-400 cursor-not-allowed"
+                        : "text-zinc-900 hover:text-zinc-100  hover:bg-zinc-900/40"
+                    }`}
+                  >
+                    <span>Generate</span>
+                  </button>
+                }
+              </div>
 
-          <div className="mt-8">
-            <ul className="space-y-2 text-xs text-zinc-500">
-              <li>
-                <p>
-                  <span className="font-semibold text-zinc-400">Your data:</span> We don't store anything you write. <a href="https://openai.com/policies/api-data-usage-policies">We do send data to Open AI.</a>
-                </p>
-              </li>
-            </ul>
+            <div className="mt-8">
+              <ul className="space-y-2 text-xs text-zinc-500">
+                <li>
+                  <p>
+                    <span className="font-semibold text-zinc-400">Your data:</span> We don't store anything you write. <a href="https://openai.com/policies/api-data-usage-policies">We do send data to Open AI.</a>
+                  </p>
+                </li>
+              </ul>
+            </div>
           </div>
         </form>
     </div>
