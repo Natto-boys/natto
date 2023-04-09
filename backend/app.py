06e9f045c8f76c758bcdbaf575cb6fc1e244d83b
jammygrams <jmahbubani@gmail.com>
2023-04-09 19:00:49 +0100

    10mb limit

--- backend/app.py
+++ backend/app.py
@@ -10,7 +10,7 @@ from SimpleChatBridge import SimpleChatBridge
 from OCR import OCR, ImageText
 
 HTTP_SERVER_PORT = 8080
-MAX_SIZE = 5*(2 ** 20) # 5 MB
+MAX_SIZE = 10*(2 ** 20) # 10 MB
 
 async def respond(ws: WebSocketServerProtocol):
     async def on_response(response: Dict[str, str]) -> None:
